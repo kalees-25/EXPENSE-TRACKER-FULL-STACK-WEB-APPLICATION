d46227fc7f52a22eb9ff12787cef5fa56ceda605
Kaleeswaran <kalees2004waran@gmail.com>
2026-06-11 11:59:33 +0530

    chore: remove old frontend folder

--- fronend_angular/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    production:false,
-    apiBaseUrl:"http://api.myapp.com"
-}
-
-// -----------THIS APP IS USED BY REAL USERS------------
-// ----------BACK-END  LIVE SERVER--------------------
-// -----------ANGULAR OPTIMIZATION (FAST)--------------------
-// ------------MINIMAL ERRORS--------------------------
--- fronend_angular/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-    production:true,
-    apiBaseUrl: "http://localhost:8000"
-};
-
-// -------------DEFAULT ENVIRONMENT FILE-------------
-// -------------FALLBACK ON DEV--------------------
-// -----------SAFETY FILE (ALWAYS WRITE)-----------
